e079f827996aa351705a5d930cfe06887000d5ea
Mohamed Khaldi <mokhaldi@c2r8p2.1337.ma>
2023-09-25 00:17:23 +0100

    mokhaldi

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: mokhaldi <mokhaldi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/21 19:19:17 by mokhaldi          #+#    #+#             */
-/*   Updated: 2023/09/21 22:55:59 by mokhaldi         ###   ########.fr       */
+/*   Updated: 2023/09/24 03:12:24 by mokhaldi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
@@ -23,14 +23,14 @@ void	ft_putnbr(int nb)
 		ft_putchar('-');
 		nb = -nb;
 	}
-	if (nb >= 10)
+	if (nb > 10)
 	{
 		ft_putnbr(nb / 10 + '0');
 		ft_putchar(nb % 10 + '0');
 	}
 	if (nb == -2147483648)
 	{
-		write(1, "2147483648", 11);
+		write(1, "2147483648", 10);
 	}
 	else
 		ft_putchar(nb + '0');
--- c01/ex00/ft_ft.c
+++ c01/ex00/ft_ft.c
@@ -1,42 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mokhaldi <mokhaldi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/19 01:39:16 by mokhaldi          #+#    #+#             */
-/*   Updated: 2023/09/23 23:09:40 by mokhaldi         ###   ########.fr       */
+/*   Created: 2023/09/21 23:49:22 by mokhaldi          #+#    #+#             */
+/*   Updated: 2023/09/21 23:54:34 by mokhaldi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-
-void	ft_print_comb2(void)
+void	ft_ft(int *nbr)
 {
-	int	a;
-	int	b;
-
-	a = 0;
-	while (a <= 98)
-	{
-		b = a + 1;
-		while (b <= 99)
-		{
-			ft_putchar(a / 10 + '0');
-			ft_putchar(a % 10 + '0');
-			ft_putchar(' ');
-			ft_putchar(b / 10 + '0');
-			ft_putchar(b % 10 + '0');
-			b++;
-			if (a != 98)
-			{
-				ft_putchar(',');
-				ft_putchar(' ');
-			}
-			else
-				ft_putchar('\n');
-		}
-		a++;
-	}
+	*nbr = 42;
 }
