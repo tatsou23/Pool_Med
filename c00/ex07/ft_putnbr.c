89294a35ca91d4be65fd139f69e592a4d7a201b9
mokhaldi <mokhaldi@student.1337.ma>
2023-09-25 06:31:54 +0100

    mokhaldi

--- c00/ex07/ft_putnbr.c
+++ c00/ex07/ft_putnbr.c
@@ -1,20 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*   test.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mokhaldi <mokhaldi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/21 19:19:17 by mokhaldi          #+#    #+#             */
-/*   Updated: 2023/09/25 02:48:58 by mokhaldi         ###   ########.fr       */
+/*   Created: 2023/09/25 06:29:16 by mokhaldi          #+#    #+#             */
+/*   Updated: 2023/09/25 06:30:23 by mokhaldi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <unistd.h>
 
-void	ft_putchar(char c)
-{
-	write(1, &c, 1);
-}
+#include <unistd.h>
 
 void	ft_putnbr(int nb)
 {
@@ -23,21 +19,11 @@ void	ft_putnbr(int nb)
 		ft_putchar('-');
 		nb = -nb;
 	}
-	if (nb > 10)
+	if (nb > 9)
 	{
-		ft_putnbr(nb / 10 + '0');
+		ft_putnbr(nb / 10);
 		ft_putchar(nb % 10 + '0');
-	}
-	if (nb == -2147483648)
-	{
-		write(1, "2147483648", 10);
 	}
 	else
 		ft_putchar(nb + '0');
-}
-
-int	main(void)
-{
-	ft_putnbr(4565);
-	return (0);
 }
