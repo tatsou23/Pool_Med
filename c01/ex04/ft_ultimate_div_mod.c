c2f63ca22abc7eacdd33dea220e148617f902da2
mokhaldi <mokhaldi@student.1337.ma>
2023-09-27 01:52:19 +0100

    mokhaldi

--- c01/ex04/ft_ultimate_div_mod.c
+++ c01/ex04/ft_ultimate_div_mod.c
@@ -6,12 +6,15 @@
 /*   By: mokhaldi <mokhaldi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 21:31:22 by mokhaldi          #+#    #+#             */
-/*   Updated: 2023/09/26 21:31:33 by mokhaldi         ###   ########.fr       */
+/*   Updated: 2023/09/27 01:52:15 by mokhaldi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_ultimate_div_mod(int *a, int *b)
 {
+	int	temp;
+
+	temp = *a;
 	*a = *a / *b;
-	*b = *a % *b;
+	*b = temp % *b;
 }
