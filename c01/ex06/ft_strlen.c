a448781a655fea3259600fa115c4184870ee86e0
mokhaldi <mokhaldi@student.1337.ma>
2023-09-27 03:02:58 +0100

    mokhaldi

--- c01/ex06/ft_strlen.c
+++ c01/ex06/ft_strlen.c
@@ -6,10 +6,28 @@
 /*   By: mokhaldi <mokhaldi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 21:48:18 by mokhaldi          #+#    #+#             */
-/*   Updated: 2023/09/26 22:24:17 by mokhaldi         ###   ########.fr       */
+/*   Updated: 2023/09/27 03:02:54 by mokhaldi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
+
 int	ft_strlen(char *str)
 {
+	int	count;
+
+	count = 0;
+	while (*str != '\0')
+	{
+		count++;
+		str++;
+	}
+	return (count);
+}
+
+int	main(int ac, char **av)
+{
+	if (ac == 2)
+		printf("Length of the string: %d\n", ft_strlen(av[1]));
+	return (0);
 }
