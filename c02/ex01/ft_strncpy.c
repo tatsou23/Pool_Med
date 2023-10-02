95988b1abc69f3afb08f57fabd2c8334f6ec37d4
mokhaldi <mokhaldi@student.1337.ma>
2023-10-02 21:01:11 +0100

    mokhaldi

--- c02/ex01/ft_strncpy.c
+++ c02/ex01/ft_strncpy.c
@@ -6,16 +6,16 @@
 /*   By: mokhaldi <mokhaldi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/26 22:28:28 by mokhaldi          #+#    #+#             */
-/*   Updated: 2023/09/28 06:20:34 by mokhaldi         ###   ########.fr       */
+/*   Updated: 2023/10/02 21:01:08 by mokhaldi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
 {
-	int	i;
+	unsigned int	i;
 
 	i = 0;
-	while (src[i] && i < n)
+	while (src[i] != '\0' && i < n)
 	{
 		dest[i] = src[i];
 		i++;
